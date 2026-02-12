760036e3f9c3c77b645f34ab9a24d96abe7bebe3
Enmanuel Balcacer <enmanuel_balcacer@bantrab.net.gt>
2026-02-12 15:29:20 -0400

    feat: initialize API with basic structure and TypeScript configuration

---
+++ apps/api/src/main.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata'
+import { NestFactory } from '@nestjs/core'
+import { Module } from '@nestjs/common'
+
+@Module({})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(4000)
+  console.log('TabbleLab API running on http://localhost:4000')
+}
+
+bootstrap()
