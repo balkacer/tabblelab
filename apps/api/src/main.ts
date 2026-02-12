9c3e082294f3a96d6643cd6e222a55dfc3be1af0
Enmanuel Balcacer <enmanuel_balcacer@bantrab.net.gt>
2026-02-12 17:16:27 -0400

    feat: enable CORS for local development

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,6 +7,11 @@ import { GlobalExceptionFilter } from './common/filters/http-exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
